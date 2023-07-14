b319012d3cea45917a2886eda1d9d36920fec967
Misaki <misaki@yamabuki.ac.jp>
2023-07-14 14:57:53 +0900

    update import

--- encrypt/__init__.py
+++ encrypt/__init__.py
@@ -2,3 +2,4 @@ from .no_crypt import NoCrypt
 from .neko_crypt import NekoCrypt
 from .encrypt_interface import EncryptInterface
 from .akabei_crypt import AkabeiCrypt
+from .hash_crypt import HashCrypt
--- structs/__init__.py
+++ structs/__init__.py
@@ -2,4 +2,4 @@ from .constants import XP3Signature
 from .file import XP3File
 from .file_index import XP3FileIndex
 from .file_entry import XP3FileEntry, XP3IndexSpecialFormat, XP3FileTime, XP3FileAdler, XP3FileSegments, XP3FileInfo
-from .encryption_parameters import encryption_parameters
+from .game_list import game_list
